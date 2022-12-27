1dc4fe1b0f98e1f1344cd8335a7b4a574ed72b8a
hariesh-sambath <shariesh2002@gmail.com>
2022-12-27 17:42:18 +0400

    added login feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Navbar from "./components/Navbar"
 import BookCard from "./components/BookCard";
 import BookGrid from "./components/BookGrid";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login"
 
 function App() {
    return (
       <div className="App">
-         <Signup />
+         <Login />
       </div>
    );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,163 @@
+import { Box, Grid, TextField, Button, Typography } from "@mui/material";
+import { useState } from "react";
+
+const Login = () => {
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+   const [usernameHelperText, setUsernameHelperText] = useState("");
+   const [passwordHelperText, setPasswordHelperText] = useState("");
+   const [usernameErr, setUsernameErr] = useState(false);
+   const [passwordErr, setPasswordErr] = useState(false);
+
+   function handleUsernameChange(e) {
+      setUsername(e.target.value);
+
+      if (!e.target.value) {
+         setUsernameErr(true);
+         setUsernameHelperText("Username cannot be empty!");
+      } else {
+         setUsernameErr(false);
+         setUsernameHelperText("");
+      }
+   }
+
+   function handlePasswordChange(e) {
+      setPassword(e.target.value);
+
+      if (!e.target.value) {
+         setPasswordErr(true);
+         setPasswordHelperText("Password cannot be empty!");
+      } else {
+         setPasswordErr(false);
+         setPasswordHelperText("");
+      }
+   }
+
+   function handleLogin(e) {
+      e.preventDefault()
+      
+      if(!username){
+         setUsernameErr(true);
+         setUsernameHelperText("Username cannot be empty!");
+      }
+      else if(!password){
+         setPasswordErr(true);
+         setPasswordHelperText("Password cannot be empty!");
+      }
+      else {
+         fetch("api/authenticate", {
+            method: "POST",
+            headers: {
+               "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({
+               username,
+               password
+            })
+         })
+         .then(res => res.json())
+         .then(data => console.log(data))
+      }
+
+   }
+
+   return (
+      <Box
+         component="main"
+         width="100vw"
+         height="100vh"
+         display="flex"
+         justifyContent="center"
+         alignItems="center"
+      >
+         <Grid
+            container
+            component="form"
+            direction="column"
+            width={400}
+            rowSpacing={4}
+            p={4}
+            border="1px solid"
+            borderRadius={10}
+            onSubmit={handleLogin}
+         >
+            <Typography
+               component="h1"
+               alignSelf="center"
+               fontFamily="Raleway, sans-serif"
+               fontSize={32}
+            >
+               Login
+            </Typography>
+            <Grid item>
+               <TextField
+                  id="username"
+                  name="username"
+                  label="Username"
+                  autoComplete="off"
+                  autoFocus
+                  fullWidth
+                  InputProps={{
+                     style: {
+                        fontFamily: "Raleway",
+                        fontSize: 18,
+                     },
+                  }}
+                  InputLabelProps={{
+                     style: {
+                        fontFamily: "Raleway",
+                        fontSize: 18,
+                     },
+                  }}
+                  onChange={handleUsernameChange}
+                  helperText={usernameHelperText}
+                  error={usernameErr}
+               ></TextField>
+            </Grid>
+            <Grid item>
+               <TextField
+                  id="password"
+                  name="password"
+                  type="password"
+                  label="Password"
+                  autoComplete="off"
+                  fullWidth
+                  InputProps={{
+                     style: {
+                        fontFamily: "Raleway",
+                        fontSize: 18,
+                     },
+                  }}
+                  InputLabelProps={{
+                     style: {
+                        fontFamily: "Raleway",
+                        fontSize: 18,
+                     },
+                  }}
+                  onChange={handlePasswordChange}
+                  helperText={passwordHelperText}
+                  error={passwordErr}
+               ></TextField>
+            </Grid>
+            <Grid item alignSelf="center">
+               <Button
+                  variant="contained"
+                  size="large"
+                  disableElevation
+                  sx={{
+                     textTransform: "none",
+                     fontFamily: "Raleway",
+                     fontSize: 16,
+                  }}
+                  disabled={usernameErr || passwordErr}
+                  type="submit"
+               >
+                  Login
+               </Button>
+            </Grid>
+         </Grid>
+      </Box>
+   );
+};
+
+export default Login;
