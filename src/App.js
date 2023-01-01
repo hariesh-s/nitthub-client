bb279e054cae17320be1618d74b58f9fa89f22b3
hariesh-sambath <shariesh2002@gmail.com>
2023-01-01 19:02:57 +0400

    matched routes with their components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Navbar from "./components/Navbar"
-import BookCard from "./components/BookCard";
+import Navbar from "./components/Navbar";
 import BookGrid from "./components/BookGrid";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login"
 
 function App() {
    return (
       <div className="App">
-         <Signup />
+         <Navbar />
+         <BookGrid />
       </div>
    );
 }
--- src/index.js
+++ src/index.js
@@ -2,42 +2,19 @@ import React, { StrictMode } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// const theme = createTheme({
-//    typography: {
-//       tab: {
-//          fontSize: 20,
-//       },
-//    },
-//    palette: {
-//       common: {
-//          purple: 'purple',
-//       },
-//    },
-//    components: {
-//       // Name of the component
-//       MuiButton: {
-//         styleOverrides: {
-//           // Name of the slot
-//           root: {
-//             // Some CSS
-//             fontSize: '1rem',
-//           },
-//         },
-//       },
-//    },
-// })
 
 const root = document.getElementById("root");
 render(
    <StrictMode>
       <BrowserRouter>
-         {/* <ThemeProvider theme={theme}> */}
          <Routes>
             <Route path="/" element={<App />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Signup />}></Route>
          </Routes>
-         {/* </ThemeProvider> */}
       </BrowserRouter>
    </StrictMode>,
    root
