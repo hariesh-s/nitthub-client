fea7ac600b4a989aadf0b611c89603e256153fa7
hariesh-sambath <shariesh2002@gmail.com>
2023-01-09 11:41:54 +0400

    bug fix

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,7 +7,7 @@ export default function AuthContextProvider({ children }) {
    const [auth, dispatchAuth] = useReducer(authReducer, { accessToken: "" });
 
    return (
-      <AuthContext.Provider value={{ ...auth, dispatchAuth }}>
+      <AuthContext.Provider value={{ auth, dispatchAuth }}>
          {children}
       </AuthContext.Provider>
    );
