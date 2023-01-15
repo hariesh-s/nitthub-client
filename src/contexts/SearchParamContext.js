1cea00056016b34ecf3801e6b3b7860091ad8afe
hariesh-sambath <shariesh2002@gmail.com>
2023-01-15 18:58:50 +0400

    slightly modified searchParam ctxt

--- src/contexts/SearchParamContext.js
+++ src/contexts/SearchParamContext.js
@@ -4,7 +4,11 @@ import searchParamReducer from "../reducers/searchParamReducer";
 export const SearchParamContext = createContext();
 
 export default function SearchParamContextProvider({ children }) {
-   const [searchParam, dispatchSearchParam] = useReducer(searchParamReducer, {});
+   const [searchParam, dispatchSearchParam] = useReducer(searchParamReducer, {
+      query: "",
+      course: "",
+      prof: "",
+   });
    console.log(searchParam);
    return (
       <SearchParamContext.Provider value={{ searchParam, dispatchSearchParam }}>
--- src/reducers/searchParamReducer.js
+++ src/reducers/searchParamReducer.js
@@ -1,7 +1,7 @@
 function searchParamReducer(state, action) {
    switch (action.type) {
       case "SEARCH":
-         return { ...state, searchParam: action.payload }
+         return { ...state, ...action.payload }
       default:
          throw Error("Unknown action " + action.type);
    }
